a0c3cc86b25618a806a9ff48cdc0bd1093ea74d3
JWooni <wjddns59@naver.com>
2022-02-28 23:02:22 +0900

    commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ str }}</div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      str: "hi",
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+  // JavaScript
+  methods: {
+    addNum: function () {},
+  },
+};
+</script>
+
+<style>
+/* CSS */
+</style>
